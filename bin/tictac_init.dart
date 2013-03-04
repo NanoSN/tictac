e918d79e24869cb495fa6b495745c0ed7d12783c
Sam Elkhateeb <same@nanosn.com>
2013-03-03 17:49:16 -0700

    adds 'init' command
    Story: 39acc0a57a tictac init: create lib, web, tools directories
    Story: 54c448379b tictac init: generate pubspec.yaml
    Story: 968ec8f03c tictac init: a command to initialize the project

---
+++ bin/tictac_init.dart
@@ -0,0 +1,45 @@
+/// initializes new project in current directory or in the specified directory
+
+/*
+  /pubspec.yaml
+  /lib/
+  /test/
+  /tools/
+ */
+
+import 'dart:io';
+
+void verify_directory_is_empty({String path: '.'}){
+  var files = new Directory(path).listSync();
+  if(!files.isEmpty) throw new Exception('this path: "$path" is not empty');
+}
+
+void create_pubspec_yaml_file({String path: '.'}){
+  var content = """
+name:  new_project
+description: new project description
+
+dependencies:
+  logging: any
+  unittest: any
+  tic:
+    path: /Users/sam/Development/Projects/tic/
+""";
+
+  var file = new File.fromPath(new Path('$path/pubspec.yaml'));
+  var sink = file.openWrite();
+  sink
+    ..addString(content)
+    ..close();
+}
+
+void create_directories(List<String> directories, {path: '.'}){
+  for(final dir in directories)
+    new Directory.fromPath(new Path('$path/$dir')).createSync(recursive: true);
+}
+
+main() {
+  verify_directory_is_empty();
+  create_pubspec_yaml_file();
+  create_directories(['lib', 'test', 'tools']);
+}
